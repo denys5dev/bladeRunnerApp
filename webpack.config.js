e9d07b755a0cadb1de1845312880c48dc02de73e
denys5dev <denys5dev@gmail.com>
2017-11-13 20:50:01 +0100

    popperjs and update to angular 5

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = (env) => {
                     test: /\.scss$/,
                     exclude: /node_modules/,
                     loaders: ['raw-loader', 'sass-loader'] // sass-loader not scss-loader
-                  }
+                }
             ]
         },
         plugins: [new CheckerPlugin()]
