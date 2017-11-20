29fd94424977b0992bda68f57681255819fff951
denys5dev <denys5dev@gmail.com>
2017-11-20 21:16:27 +0100

    register form

--- ClientApp/app/routes/pages/register/register.component.ts
+++ ClientApp/app/routes/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'registration-app',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RegisterComponent implements OnInit {
-    constructor() { }
+    
+    registerForm: FormGroup;
+
+    constructor(private _fb: FormBuilder) { 
+        this.registerForm = this._fb.group({
+            firstName: ['', [Validators.minLength(1), Validators.required]],
+            lastName: ['', [Validators.minLength(1), Validators.required]],
+            email: ['', [Validators.email, Validators.required]],
+            password: ['', [Validators.minLength(6), Validators.required]],
+            confimPassword: ['', [Validators.required]]
+        });
+    }
 
     ngOnInit() { }
+
+    onSubmitRegisterForm(form: any) {
+        console.log(form)
+    }
 }
